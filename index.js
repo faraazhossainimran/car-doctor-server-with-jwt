f63bf28c812ab32b9adff28284f09d93ca1c42a3
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-11-03 23:44:46 +0600

    get data from database just for email specific bookings

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -25,13 +25,42 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const serviceCollection = client.db('car-doctor').collection('services')
+    const bookingCollection = client.db('car-doctor').collection('bookings')
+    // get services
     app.get('/services', async(req, res)=> {
       const cursor = serviceCollection.find(); 
       const result = await cursor.toArray();
       res.send(result)
     })
     
-
+    // get a single service
+    app.get('/services/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const options = {
+        // Include only the `title` and `imdb` fields in each returned document
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+      };
+      const result = await serviceCollection.findOne(query, options)
+      res.send(result)
+    })
+    // get a/some bookings
+    app.get('/bookings', async(req, res)=> {
+      console.log(req.query.email);
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray()
+      res.send(result)
+    })
+    // post bookings
+    app.post('/bookings', async(req, res)=> {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking)
+      res.send(result)
+    }) 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
